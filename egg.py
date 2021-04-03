af205ebc2c27278a43a32bec771fbf7bf4aa4e45
Eric-Nicolas <randomimus.beakuzz@gmail.com>
2021-04-03 18:42:28 +0200

    Made the basket's hitbox smaller

--- egg.py
+++ egg.py
@@ -15,22 +15,22 @@ class Egg(pygame.sprite.Sprite):
             pygame.image.load(os.path.join('assets', 'chocolate_egg.png')),
             (50, 50)
         )
-        self._rect = self._IMG.get_rect()
-        self._rect.x = random.randint(0, self._WIN_WIDTH - self._IMG.get_width())
-        self._rect.y = 0
+        self.rect = self._IMG.get_rect()
+        self.rect.x = random.randint(0, self._WIN_WIDTH - self._IMG.get_width())
+        self.rect.y = 0
         self._speed = 2
 
     def go_top(self, score_bar):
-        self._rect.y = 0
-        self._rect.x = random.randint(0, self._WIN_WIDTH - self._IMG.get_width())
+        self.rect.y = 0
+        self.rect.x = random.randint(0, self._WIN_WIDTH - self._IMG.get_width())
         if score_bar.is_winning() and score_bar.get_score() % 5 == 0:
             self._speed += 1
 
     def fall(self):
-        self._rect.y += self._speed
+        self.rect.y += self._speed
 
     def has_fallen(self):
-        return self._rect.y >= self._WIN_HEIGHT - self._IMG.get_height() - 75
+        return self.rect.y >= self._WIN_HEIGHT - self._IMG.get_height() - 75
 
     def draw(self, window):
-        window.blit(self._IMG, self._rect)
+        window.blit(self._IMG, self.rect)
