cf07e39ffe494aaee9ab9a16d8dd7da7f6fa76e7
Eric-Nicolas <randomimus.beakuzz@gmail.com>
2021-04-03 18:54:33 +0200

    Changed score bar behavior

--- score_bar.py
+++ score_bar.py
@@ -4,30 +4,32 @@ import pygame
 
 class ScoreBar:
     def __init__(self, window):
-        self.color = (87, 64, 53)
-        self.bg_color = (135, 147, 154)
-        self.default_width = window.get_width() // 2
-        self.thickness = 10
+        self._WIN = window
+        self._COLOR = (87, 64, 53)
+        self._BG_COLOR = (135, 147, 154)
+        self._DEFAULT_WIDTH = window.get_width() // 2
+        self._THICKNESS = 10
 
         self.score = 0
-        self.width = self.default_width
+        self._width = self._DEFAULT_WIDTH
 
     def get_score(self):
         return self.score
 
     def is_winning(self):
-        return self.width > self.default_width
+        return self._width > self._DEFAULT_WIDTH
 
     def is_empty(self):
-        return self.width <= 0
+        return self._width <= 0
 
     def increase_score(self):
-        self.score += 1
+        if self._width < self._WIN.get_width():
+            self.score += 1
 
     def decrease_score(self):
         self.score -= 1
 
     def draw(self, window):
-        self.width = self.default_width + self.score * 200
-        pygame.draw.rect(window, self.bg_color, (0, 0, window.get_width(), self.thickness))
-        pygame.draw.rect(window, self.color, (0, 0, self.width, self.thickness))
+        self._width = self._DEFAULT_WIDTH + self.score * 200
+        pygame.draw.rect(window, self._BG_COLOR, (0, 0, window.get_width(), self._THICKNESS))
+        pygame.draw.rect(window, self._COLOR, (0, 0, self._width, self._THICKNESS))
