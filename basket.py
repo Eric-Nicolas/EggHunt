fa1002993a7a3e67b15ac45609e67291ef8a3b6e
Eric-Nicolas <randomimus.beakuzz@gmail.com>
2021-04-11 15:52:21 +0200

    Changed basket speed

--- basket.py
+++ basket.py
@@ -17,7 +17,7 @@ class Basket(pygame.sprite.Sprite):
         self.rect.x = (self._WIN_WIDTH - self._IMG.get_width()) // 2
         self.rect.y = window.get_height() - self._IMG.get_height() - 75
         self._x_change = 0
-        self._SPEED = 10
+        self._SPEED = 5
         self._hitbox = (self.rect.x, self.rect.y + self._IMG.get_height() // 2, self._IMG.get_width(), self._IMG.get_height() // 2)
         self.rect = pygame.Rect(self._hitbox)
 
